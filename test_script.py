c1bf3e4074d853c395e777685832524a76650af2
Tahzib Mashrik <mashrikt@gmail.com>
2020-10-22 19:26:50 +0600

    Add start and end date specific tests

--- test_script.py
+++ test_script.py
@@ -57,8 +57,29 @@ class TestSuggestedTime:
         result = ["08:00-14:00"]
         assert suggested_time(**parameters) == result
 
-    def test_smaller_duration_gap_ignored(self, parameters):
-        parameters["list_1"] = ["9:00-10:00", "10:05-11:00"]
-        parameters["list_2"] = []
-        result = ["08:00-09:00", "11:00-17:00"]
+    @pytest.mark.parametrize(
+        "duration,result",
+        [
+            (5, ["08:00-09:00", "10:00-10:05", "11:00-17:00"]),
+            (15, ["08:00-09:00", "11:00-17:00"]),
+            (60, ["08:00-09:00", "11:00-17:00"]),
+        ]
+    )
+    def test_smaller_duration_gaps(self, parameters, duration, result):
+        parameters["list_1"] = ["9:00-10:00"]
+        parameters["list_2"] = ["10:05-11:00"]
+        parameters["duration"] = duration
+        assert suggested_time(**parameters) == result
+
+    @pytest.mark.parametrize(
+        "start_time,end_time,result",
+        [
+            ("09:00", "19:00", ["12:30-14:00", "15:30-19:00"]),
+            ("08:00", "23:59", ["08:00-09:00", "12:30-14:00", "15:30-23:59"]),
+            ("00:00", "23:59", ["00:00-09:00", "12:30-14:00", "15:30-23:59"]),
+        ]
+    )
+    def test_varying_start_end_time(self, parameters, start_time, end_time, result):
+        parameters["start_time"] = start_time
+        parameters["end_time"] = end_time
         assert suggested_time(**parameters) == result
